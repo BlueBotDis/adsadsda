3199e53a40047481a6c3fb4ef261b81a11e482d1
karzo8s <44073963+karzo8s@users.noreply.github.com>
2018-11-07 13:43:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -43,7 +43,7 @@ function timerFunc() {
   
   
     client.on('message', msg => {//Rie
-        client.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(700)) // 
+        client.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(70)) // 
 
 
     });

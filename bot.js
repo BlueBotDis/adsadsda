63c7f9e5fa5a4efa2865b186a0bdb23361a199d4
karzo8s <44073963+karzo8s@users.noreply.github.com>
2018-11-08 11:23:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -81,7 +81,7 @@ function timerFunc() {
 
 
 
-var timer = setTimeout(timerFunc,700);//Good Speed : 870 Very Fast : 305
+var timer = setTimeout(timerFunc, 700);//Good Speed : 870 Very Fast : 305
 
 client.on('ready', () => console.log('ProBot Credits Miner v2'));//كلام يظهر فل كونسول
 
